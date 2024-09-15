2d9cbf1108804497bf211bd57160e1c5592bd413
Konrad ALAHASSA <am.konrad21@gmail.com>
2024-09-15 18:54:02 -0400

    Fix import of visualize_time_series

--- eeg_interface/utils/visualize.py
+++ eeg_interface/utils/visualize.py
@@ -3,6 +3,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy import stats
 
-def visualizeTimeSeries(df):
+def visualize_time_series(df):
     graph = sns.lineplot(data=df).get_figure()
     graph.savefig('timeSeries.png')
